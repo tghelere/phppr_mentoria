a5f899cf9551e69512fbaaa4ce04121e19f010e2
tghelere <ghelere@outlook.com>
2020-08-15 02:05:28 -0300

    Users migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,15 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('timezone')->default('America/Sao_Paulo');
+            $table->string('social_n_work')->comment('social networks, website, github')->nullable();
+            $table->string('knowledges')->nullable();
+            $table->enum('status', ['active', 'inactive', 'awaiting'])->default('awaiting');
             $table->rememberToken();
             $table->timestamps();
         });
