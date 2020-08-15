81eac9db7161c4b43c0edd8adaf1fdd9d054a0f9
tghelere <ghelere@outlook.com>
2020-08-15 02:10:49 -0300

    Users controller and model pre configs

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+
+class UserController extends Controller
+{
+    public function login()
+    {
+    }
+    public function logout()
+    {
+    }
+    public function list()
+    {
+    }
+    public function show()
+    {
+    }
+    public function create(Request $request)
+    {
+    }
+    public function update(Request $request)
+    {
+    }
+    public function delete()
+    {
+    }
+}
--- app/User.php
+++ app/User.php
@@ -2,14 +2,10 @@
 
 namespace App;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Model;
 
-class User extends Authenticatable
+class User extends Model
 {
-    use Notifiable;
-
     /**
      * The attributes that are mass assignable.
      *
